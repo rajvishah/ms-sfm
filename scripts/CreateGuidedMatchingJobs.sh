00be3306b44755a1dd78a1194744ac7d2b8153a5
rajvi <rajvi.a.shah@gmail.com>
2015-06-23 21:27:28 +0530

    Added script to create guided matching jobs

---
+++ scripts/CreateGuidedMatchingJobs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Run this script from the directory where you wish to create slurm jobs
+# Run this script to create various files necessary to run guided matching
+# BUNDLER_DIR (previous reconstruction directory)
+# BASE_DIR (directory where you want to store the matching outcome)
+# IMAGE_DIR (where images & keys are stored)
+# NUM_NEAR (number of neighbouring images to match)
+# NUM_LISTS ( number of parallel lists (usually 212) )
+
+BUNDLER_DIR=$1
+BASE_DIR=$2
+IMAGE_DIR=$3
+NUM_NEAR=$4
+NUM_LISTS=$5
+BIN_PATH=$6
+
+echo "USAGE : BUNDLER_DIR BASE_DIR IMAGE_DIR NUM_NEAR NUM_LISTS BIN_PATH"
+
+RESULT_DIR=$BASE_DIR/guided_match
+mkdir $RESULT_DIR
+
+cp $BUNDLER_DIR/bundle/bundle.out $BASE_DIR/bundle.out
+cp $BUNDLER_DIR/list_tmp.txt $BASE_DIR/list_images.txt
+cp $BUNDLER_DIR/list_keys.txt $BASE_DIR/list_keys.txt
+identify $IMAGE_DIR/*.jpg | cut -d' ' -f3 | sed 's/x/ /g'> $BASE_DIR/images_dims.txt
+
+# For the first iteration, do not pass --query_list
+# For later iterations, pass the list of newly localized images as --query_list
+
+#../bin/CreateGuidedMatchPairs --base_dir=$BASE_DIR --result_dir=$RESULT_DIR --nn_images=$NUM_NEAR --num_lists=$NUM_LISTS --query_list=$1/query_ids.txt
+
+$BIN_PATH/CreateGuidedMatchPairs --base_dir=$BASE_DIR --result_dir=$RESULT_DIR --nn_images=$NUM_NEAR --num_lists=$NUM_LISTS
+
+
+LIST_DIR=$RESULT_DIR"/matching_lists/"
+MATCHES_DIR=$RESULT_DIR"/matches_files/"
+
+pairListFiles=($LIST_DIR/pairs-*.txt)
+filecounter=-1
+counter=0
+numCores=24
+filename=""
+for file in ${pairListFiles[@]}
+do
+    if [ `expr $counter % $numCores` -eq 0 ];
+    then
+        echo "wait" >> $filename
+        counter=0
+        filecounter=`expr $filecounter + 1`
+        filename="job_"$filecounter".sh"
+        echo $filename
+        touch $filename
+        echo "#!/bin/bash" > $filename
+        echo "#SBATCH -A cvit" >> $filename
+        echo "#SBATCH -p cvit" >> $filename
+        echo "#SBATCH -n 24" >> $filename
+        echo "#SBATCH --mem-per-cpu=3945" >> $filename
+        echo "#SBATCH -t 24:00:00" >> $filename
+    fi 
+    echo "time "$BIN_PATH/GuidedMatchLists" --base_dir="$BASE_DIR" --result_dir="$MATCHES_DIR" --list_file="$file" 2>&1 &" >> $filename
+    counter=`expr $counter + 1`
+done
